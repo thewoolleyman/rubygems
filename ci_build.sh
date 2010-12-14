35b5181c72bc0da48318a7b5ad626897164ae08d
Chad Woolley <thewoolleyman@gmail.com>
2010-12-13 20:40:27 -0700

    ci updates, allow different interpreters

---
+++ ci_build.sh
@@ -0,0 +1,20 @@
+# You must ensure that rake, hoe, and minitest are already installed in specified RVM interpreter
+export rvm_install_on_use_flag=1
+export rvm_gemset_create_on_use_flag=1
+source $HOME/.rvm/environments/ruby-$1
+rvm use $1
+# TODO: How can you make Hoe's check_extra_deps not use sudo?
+#rake check_extra_deps default
+rake default
+
+
+echo
+echo "[CruiseControl] Build environment:"
+echo "[CruiseControl]   $(cat /etc/issue)"
+echo "[CruiseControl]   $(uname -a)"
+echo "[CruiseControl]   $(ruby -v)"
+echo "[CruiseControl]   gem env:"
+gem env
+echo "[CruiseControl]   gem list:"
+gem list
+
