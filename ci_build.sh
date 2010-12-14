2a5260d10f84d474b73e3b2e55c06159da451c2d
Chad Woolley <thewoolleyman@gmail.com>
2010-12-13 20:56:39 -0700

    trying to make rvm work in ccrb

--- ci_build.sh
+++ ci_build.sh
@@ -2,17 +2,16 @@
 export rvm_install_on_use_flag=1
 export rvm_gemset_create_on_use_flag=1
 source $HOME/.rvm/environments/ruby-$1
-rvm use $1
 
-echo
-echo "[CruiseControl] Build environment:"
-echo "[CruiseControl]   $(cat /etc/issue)"
-echo "[CruiseControl]   $(uname -a)"
-echo "[CruiseControl]   $(ruby -v)"
-echo "[CruiseControl]   gem env:"
+
+echo "----------------------------"
+echo "Build environment:"
+cat /etc/issue
+uname -a
+ruby -v
 gem env
-echo "[CruiseControl]   gem list:"
 gem list
+echo "----------------------------"
 
 # TODO: How can you make Hoe's check_extra_deps not use sudo?
 #rake check_extra_deps default
